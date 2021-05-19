394eaba467d2f484eb504efe04980ac3a57fc253
zzZ5 <baoju_liu@foxmail.com>
2021-05-19 17:26:24 +0800

    给active添加一些待用的表

--- src/views/dashboard/active/index.vue
+++ src/views/dashboard/active/index.vue
@@ -5,10 +5,10 @@
         Your roles:
         <span v-for="item in roles" :key="item" class="pan-info-roles">{{ item }}</span>
       </pan-thumb>
-      <github-corner style="position: absolute; top: 0px; border: 0; right: 0;" />
+      <!-- <github-corner style="position: absolute; top: 0px; border: 0; right: 0;" /> -->
       <div class="info-container">
-        <span class="display_name">{{ name }}</span>
-        <span style="font-size:20px;padding-top:20px;display:inline-block;">Editor's Dashboard</span>
+        <span class="display_name">{{ username }}</span>
+        <span style="font-size:20px;padding-top:20px;display:inline-block;">Active's Dashboard</span>
       </div>
     </div>
     <div>
@@ -20,11 +20,11 @@
 <script>
 import { mapGetters } from 'vuex'
 import PanThumb from '@/components/PanThumb'
-import GithubCorner from '@/components/GithubCorner'
+// import GithubCorner from '@/components/GithubCorner'
 
 export default {
   name: 'DashboardActive',
-  components: { PanThumb, GithubCorner },
+  components: { PanThumb },
   data() {
     return {
       emptyGif: 'https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3'
@@ -32,7 +32,7 @@ export default {
   },
   computed: {
     ...mapGetters([
-      'name',
+      'username',
       'avatar',
       'roles'
     ])
