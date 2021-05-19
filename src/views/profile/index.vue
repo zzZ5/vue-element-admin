e50cacb43aab9b3f9ec8047cb8f535779e1f4be8
zzZ5 <baoju_liu@foxmail.com>
2021-05-20 00:23:18 +0800

    新增record界面

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -10,15 +10,15 @@
         <el-col :span="18" :xs="24">
           <el-card>
             <el-tabs v-model="activeTab">
-              <!-- <el-tab-pane label="Activity" name="activity">
-                <activity />
-              </el-tab-pane>
-              <el-tab-pane label="Timeline" name="timeline">
-                <timeline />
-              </el-tab-pane> -->
               <el-tab-pane label="Account" name="account">
                 <account :user="user" />
               </el-tab-pane>
+              <el-tab-pane label="Record" name="record">
+                <record />
+              </el-tab-pane>
+              <!-- <el-tab-pane label="Activity" name="activity">
+                <activity />
+              </el-tab-pane> -->
             </el-tabs>
           </el-card>
         </el-col>
@@ -31,13 +31,13 @@
 <script>
 import { mapGetters } from 'vuex'
 import UserCard from './components/UserCard'
-// import Activity from './components/Activity'
-// import Timeline from './components/Timeline'
 import Account from './components/Account'
+import Record from './components/Record'
+// import Activity from './components/Activity'
 
 export default {
   name: 'Profile',
-  components: { UserCard, Account },
+  components: { UserCard, Account, Record },
   data() {
     return {
       user: {},
