21b4600f2348fa0d13759175eb37dcfb246b99f4
zzZ5 <baoju_liu@foxmail.com>
2021-05-20 17:32:56 +0800

    新增equipment api

---
+++ src/api/eqeuipment.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+export function fetchList(query) {
+  return request({
+    url: '/equipment/list/',
+    method: 'get',
+    params: query
+  })
+}
+
+export function fetchEquipment(id) {
+  return request({
+    url: '/vue-element-admin/' + id + '/info/',
+    method: 'get'
+  })
+}
+
+export function fetchRecord(id, listQuery) {
+  return request({
+    url: '/equipment/' + id + '/record/',
+    method: 'get',
+    params: { listQuery }
+  })
+}
+
+export function createEquipment(data) {
+  return request({
+    url: '/equipment/create/',
+    method: 'post',
+    data
+  })
+}
+
+export function updateArticle(id, data) {
+  return request({
+    url: '/equipment/' + id + '/update/',
+    method: 'post',
+    data
+  })
+}
