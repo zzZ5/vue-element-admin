e53f3636cbc43044dd5bbc484cb9e2c640cc5c47
zzZ5 <baoju_liu@foxmail.com>
2021-05-26 15:12:13 +0800

    新建设备页面

---
+++ src/views/equipment/create.vue
@@ -0,0 +1,13 @@
+<template>
+  <equipment-detail :is-edit="false" />
+</template>
+
+<script>
+import EquipmentDetail from './components/EquipmentDetail'
+
+export default {
+  name: 'CreateEquipment',
+  components: { EquipmentDetail }
+}
+</script>
+
