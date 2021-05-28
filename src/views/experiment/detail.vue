f97d5527f57830d260a0b5e08f743ccd771ad6dd
zzZ5 <baoju_liu@foxmail.com>
2021-05-28 17:24:27 +0800

    实验内容入口

---
+++ src/views/experiment/detail.vue
@@ -0,0 +1,12 @@
+<template>
+  <Experiment-detail :is-edit="true" />
+</template>
+
+<script>
+import ExperimentDetail from './components/ExperimentDetail'
+
+export default {
+  name: 'EditForm',
+  components: { ExperimentDetail }
+}
+</script>
